73050b9756c61b0daa0ce9392e6c7618ff9079e2
Nati <leh.natalia.szilvia@blathy.info>
2026-03-24 17:08:40 +0100

    Book class added

---
+++ BookCatalog/BookCatalog/BookCatalog/Book.cs
@@ -0,0 +1,32 @@
+﻿namespace BookCatalog
+{
+    internal class Book
+    {
+       
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; } 
+        public int Year { get; set; }         
+        public bool IsFavorite { get; set; }  
+
+        public Book(int id, string title, string author, string category, string description, int rating, int year, bool isFavorite)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Category = category;
+            Description = description;
+            Rating = rating;
+            Year = year;
+            IsFavorite = isFavorite;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}. {Title} ({Author}) - {Category} - Értékelés: {Rating}/10 - Kiadás: {Year} {(IsFavorite ? "[Kedvenc]" : "")}";
+        }
+    }
+}
